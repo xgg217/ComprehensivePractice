6b546356cef17a3bfebf70c1b7ea814e633a5a79
付祖敏 <1326580471@qq.com>
2025-11-08 11:26:34 +0800

    调整

--- src/router/webApi.ts
+++ src/router/webApi.ts
@@ -1,6 +1,5 @@
 import type { RouteRecordRaw } from "vue-router";
 import layout from "@/layout/index.vue";
-// import aa from "@/views/ThreeJS/Examples/aa.vue";
 
 const row: RouteRecordRaw = {
   path: "/WebApi",
@@ -22,7 +21,7 @@ const row: RouteRecordRaw = {
       },
     },
 
-    // #region 集合
+    // #region API 集合
     {
       path: "Collection",
       name: "WebApiCollection",
@@ -47,8 +46,7 @@ const row: RouteRecordRaw = {
         {
           path: "WebWorker1",
           name: "WebApiCollectionWebWorker1",
-          component: () =>
-            import("@/views/WebApi/Collection/WebWorker/index.vue"),
+          component: () => import("@/views/WebApi/Collection/WebWorker/index.vue"),
           meta: {
             title: "Web Worker",
             imgSrc: "",
@@ -57,10 +55,27 @@ const row: RouteRecordRaw = {
         {
           path: "WebWorker2",
           name: "WebApiCollectionWebWorker2",
-          component: () =>
-            import("@/views/WebApi/Collection/WebWorker/File.vue"),
+          component: () => import("@/views/WebApi/Collection/WebWorker/File.vue"),
           meta: {
             title: "Web Worker之文件分片",
+            imgSrc: "",
+          },
+        },
+        {
+          path: "WebAnimations1",
+          name: "WebApiCollectionWebAnimations1",
+          component: () => import("@/views/WebApi/Collection/WebAnimations/index.vue"),
+          meta: {
+            title: "Web Animations API",
+            imgSrc: "",
+          },
+        },
+        {
+          path: "IntersectionObserver1",
+          name: "WebApiCollectionIntersectionObserver1",
+          component: () => import("@/views/WebApi/Collection/IntersectionObserver/index.vue"),
+          meta: {
+            title: "IntersectionObserver 元素可见性",
             imgSrc: "",
           },
         },
@@ -193,8 +208,7 @@ const row: RouteRecordRaw = {
         {
           path: "ImagesAndAnimations",
           name: "CanvasImagesAndAnimations",
-          component: () =>
-            import("@/views/WebApi/Canvas/ImagesAndAnimations.vue"),
+          component: () => import("@/views/WebApi/Canvas/ImagesAndAnimations.vue"),
           meta: {
             title: "图像与动画",
             imgSrc: "ImagesAndAnimations.webp",
@@ -249,8 +263,7 @@ const row: RouteRecordRaw = {
         {
           path: "AvatarCroppingAndUploading",
           name: "CanvasAvatarCroppingAndUploading",
-          component: () =>
-            import("@/views/WebApi/Canvas/AvatarCroppingAndUploading.vue"),
+          component: () => import("@/views/WebApi/Canvas/AvatarCroppingAndUploading.vue"),
           meta: {
             title: "头像裁剪上传",
             imgSrc: "",
@@ -259,8 +272,7 @@ const row: RouteRecordRaw = {
         {
           path: "GlobalCompositeOperation",
           name: "CanvasGlobalCompositeOperation",
-          component: () =>
-            import("@/views/WebApi/Canvas/GlobalCompositeOperation.vue"),
+          component: () => import("@/views/WebApi/Canvas/GlobalCompositeOperation.vue"),
           meta: {
             title: "图形合成之路径合成",
             imgSrc: "",
@@ -270,8 +282,7 @@ const row: RouteRecordRaw = {
         {
           path: "GlobalCompositeOperationColor",
           name: "CanvasGlobalCompositeOperationColor",
-          component: () =>
-            import("@/views/WebApi/Canvas/GlobalCompositeOperationColor.vue"),
+          component: () => import("@/views/WebApi/Canvas/GlobalCompositeOperationColor.vue"),
           meta: {
             title: "图形合成之颜色合成",
             imgSrc: "",
@@ -290,8 +301,7 @@ const row: RouteRecordRaw = {
         {
           path: "DynamicClock",
           name: "CanvasDynamicClock",
-          component: () =>
-            import("@/views/WebApi/Canvas/DynamicClock/index.vue"),
+          component: () => import("@/views/WebApi/Canvas/DynamicClock/index.vue"),
           meta: {
             title: "动态时钟",
             imgSrc: "DynamicClock/DynamicClock.gif",
@@ -327,8 +337,7 @@ const row: RouteRecordRaw = {
         {
           path: "OnlineSignature",
           name: "CanvasOnlineSignature",
-          component: () =>
-            import("@/views/WebApi/Canvas/OnlineSignature/index.vue"),
+          component: () => import("@/views/WebApi/Canvas/OnlineSignature/index.vue"),
           meta: {
             title: "在线签名、在线画板",
             imgSrc: "OnlineSignature/OnlineSignature.png",
@@ -337,8 +346,7 @@ const row: RouteRecordRaw = {
         {
           path: "DrawingBoard",
           name: "CanvasDrawingBoard",
-          component: () =>
-            import("@/views/WebApi/Canvas/DrawingBoard/index.vue"),
+          component: () => import("@/views/WebApi/Canvas/DrawingBoard/index.vue"),
           meta: {
             title: "画板",
             imgSrc: "DrawingBoard/画板.png",
@@ -399,8 +407,7 @@ const row: RouteRecordRaw = {
         {
           path: "CustomsSceneSolarSystem",
           name: "WebApiThreeJSCustomsCustomsSceneSolarSystem",
-          component: () =>
-            import("@/views/WebApi/ThreeJSCustoms/SceneSolarSystem/index.vue"),
+          component: () => import("@/views/WebApi/ThreeJSCustoms/SceneSolarSystem/index.vue"),
           meta: {
             title: "场景之太阳系、太阳、地球、月亮",
             imgSrc: "SceneSolarSystem/SceneSolarSystem.png",
@@ -409,8 +416,7 @@ const row: RouteRecordRaw = {
         {
           path: "Camera1",
           name: "WebApiThreeJSCustomsCamera1",
-          component: () =>
-            import("@/views/WebApi/ThreeJSCustoms/Camera1/index.vue"),
+          component: () => import("@/views/WebApi/ThreeJSCustoms/Camera1/index.vue"),
           meta: {
             title: "相机之相机属性",
             imgSrc: "Camera1/Camera1.jpg",
@@ -420,8 +426,7 @@ const row: RouteRecordRaw = {
         {
           path: "Camera2",
           name: "WebApiThreeJSCustomsCamera2",
-          component: () =>
-            import("@/views/WebApi/ThreeJSCustoms/Camera2/index.vue"),
+          component: () => import("@/views/WebApi/ThreeJSCustoms/Camera2/index.vue"),
           meta: {
             title: "相机之局部渲染",
             imgSrc: "Camera2/Camera2.png",
@@ -431,8 +436,7 @@ const row: RouteRecordRaw = {
         {
           path: "Camera3",
           name: "WebApiThreeJSCustomsCamera3",
-          component: () =>
-            import("@/views/WebApi/ThreeJSCustoms/Camera3/index.vue"),
+          component: () => import("@/views/WebApi/ThreeJSCustoms/Camera3/index.vue"),
           meta: {
             title: "待定 双屏渲染 StereoEffect ",
             imgSrc: "Camera3/Camera3.jpg",
@@ -441,8 +445,7 @@ const row: RouteRecordRaw = {
         {
           path: "Camera4",
           name: "WebApiThreeJSCustomsCamera4",
-          component: () =>
-            import("@/views/WebApi/ThreeJSCustoms/Camera4/index.vue"),
+          component: () => import("@/views/WebApi/ThreeJSCustoms/Camera4/index.vue"),
           meta: {
             title: "StereoCamera 立方相机",
             imgSrc: "Camera4/Camera4.jpg",
@@ -451,8 +454,7 @@ const row: RouteRecordRaw = {
         {
           path: "Camera5",
           name: "WebApiThreeJSCustomsCamera5",
-          component: () =>
-            import("@/views/WebApi/ThreeJSCustoms/Camera5/index.vue"),
+          component: () => import("@/views/WebApi/ThreeJSCustoms/Camera5/index.vue"),
           meta: {
             title: "StereoCamera 之 3D立体影像",
             imgSrc: "Camera5/Camera5.jpg",
@@ -462,8 +464,7 @@ const row: RouteRecordRaw = {
         {
           path: "Camera6",
           name: "WebApiThreeJSCustomsCamera6",
-          component: () =>
-            import("@/views/WebApi/ThreeJSCustoms/Camera6/index.vue"),
+          component: () => import("@/views/WebApi/ThreeJSCustoms/Camera6/index.vue"),
           meta: {
             title: "待定",
             imgSrc: "Camera6/Camera6.jpg",
@@ -473,8 +474,7 @@ const row: RouteRecordRaw = {
         {
           path: "texture",
           name: "WebApiThreeJSCustomsTexture",
-          component: () =>
-            import("@/views/WebApi/ThreeJSCustoms/Texture/index.vue"),
+          component: () => import("@/views/WebApi/ThreeJSCustoms/Texture/index.vue"),
           meta: {
             title: "纹理+批量加载",
             imgSrc: "Texture/Texture.gif",
@@ -483,10 +483,7 @@ const row: RouteRecordRaw = {
         {
           path: "CubeTextureLoader1",
           name: "WebApiThreeJSCustomsCubeTextureLoader1",
-          component: () =>
-            import(
-              "@/views/WebApi/ThreeJSCustoms/CubeTextureLoader1/index.vue"
-            ),
+          component: () => import("@/views/WebApi/ThreeJSCustoms/CubeTextureLoader1/index.vue"),
           meta: {
             title: "环境贴图",
             imgSrc: "CubeTextureLoader1/CubeTextureLoader1.png",
@@ -497,9 +494,7 @@ const row: RouteRecordRaw = {
           path: "shadowMapsSimulatedShadow",
           name: "WebApiThreeJSCustomsShadowMapsSimulatedShadow",
           component: () =>
-            import(
-              "@/views/WebApi/ThreeJSCustoms/ShadowMapsSimulatedShadow/index.vue"
-            ),
+            import("@/views/WebApi/ThreeJSCustoms/ShadowMapsSimulatedShadow/index.vue"),
           meta: {
             title: "阴影之假阴影",
             imgSrc: "ShadowMapsSimulatedShadow/ShadowMapsSimulatedShadow.png",
@@ -509,10 +504,7 @@ const row: RouteRecordRaw = {
         {
           path: "shadowMapsPointLight",
           name: "WebApiThreeJSCustomsShadowMapsPointLight",
-          component: () =>
-            import(
-              "@/views/WebApi/ThreeJSCustoms/ShadowMapsPointLight/index.vue"
-            ),
+          component: () => import("@/views/WebApi/ThreeJSCustoms/ShadowMapsPointLight/index.vue"),
           meta: {
             title: "阴影之聚光灯",
             imgSrc: "ShadowMapsPointLight/ShadowMapsPointLight.png",
@@ -521,8 +513,7 @@ const row: RouteRecordRaw = {
         {
           path: "fog",
           name: "WebApiThreeJSCustomsFog",
-          component: () =>
-            import("@/views/WebApi/ThreeJSCustoms/Fog/index.vue"),
+          component: () => import("@/views/WebApi/ThreeJSCustoms/Fog/index.vue"),
           meta: {
             title: "雾",
             imgSrc: "Fog/Fog.gif",
@@ -532,9 +523,7 @@ const row: RouteRecordRaw = {
           path: "GenerateMountainTerrain",
           name: "WebApiThreeJSCustomsGenerateMountainTerrain",
           component: () =>
-            import(
-              "@/views/WebApi/ThreeJSCustoms/GenerateMountainTerrain/index.vue"
-            ),
+            import("@/views/WebApi/ThreeJSCustoms/GenerateMountainTerrain/index.vue"),
           meta: {
             title: "生成山脉地形",
             imgSrc: "GenerateMountainTerrain/生成山脉地形.gif",
@@ -544,8 +533,7 @@ const row: RouteRecordRaw = {
         {
           path: "EdgesGeometry",
           name: "WebApiThreeJSCustomsEdgesGeometry",
-          component: () =>
-            import("@/views/WebApi/ThreeJSCustoms/EdgesGeometry/index.vue"),
+          component: () => import("@/views/WebApi/ThreeJSCustoms/EdgesGeometry/index.vue"),
           meta: {
             title: "虚线线框几何体",
             imgSrc: "EdgesGeometry/EdgesGeometry.png",
@@ -554,8 +542,7 @@ const row: RouteRecordRaw = {
         {
           path: "TunnelShuttle",
           name: "WebApiThreeJSCustomsTunnelShuttle",
-          component: () =>
-            import("@/views/WebApi/ThreeJSCustoms/TunnelShuttle/index.vue"),
+          component: () => import("@/views/WebApi/ThreeJSCustoms/TunnelShuttle/index.vue"),
           meta: {
             title: "隧道穿梭",
             imgSrc: "TunnelShuttle/TunnelShuttle.png",
@@ -564,8 +551,7 @@ const row: RouteRecordRaw = {
         {
           path: "Box3",
           name: "WebApiThreeJSCustomsBox3",
-          component: () =>
-            import("@/views/WebApi/ThreeJSCustoms/Box3/index.vue"),
+          component: () => import("@/views/WebApi/ThreeJSCustoms/Box3/index.vue"),
           meta: {
             title: "包围盒 Box3",
             imgSrc: "Box3/Box3.png",
@@ -575,8 +561,7 @@ const row: RouteRecordRaw = {
         {
           path: "BufferGeometry1",
           name: "WebApiThreeJSCustomsBufferGeometry1",
-          component: () =>
-            import("@/views/WebApi/ThreeJSCustoms/BufferGeometry1/index.vue"),
+          component: () => import("@/views/WebApi/ThreeJSCustoms/BufferGeometry1/index.vue"),
           meta: {
             title: "缓冲模型 BufferGeometry之三角形绘制",
             imgSrc: "BufferGeometry1/BufferGeometry1.png",
@@ -585,8 +570,7 @@ const row: RouteRecordRaw = {
         {
           path: "BufferGeometry2",
           name: "WebApiThreeJSCustomsBufferGeometry2",
-          component: () =>
-            import("@/views/WebApi/ThreeJSCustoms/BufferGeometry2/index.vue"),
+          component: () => import("@/views/WebApi/ThreeJSCustoms/BufferGeometry2/index.vue"),
           meta: {
             title: "缓冲模型 BufferGeometry之points点绘制",
             // imgSrc: "Box3/Box3.png",
@@ -595,8 +579,7 @@ const row: RouteRecordRaw = {
         {
           path: "BufferGeometry3",
           name: "WebApiThreeJSCustomsBufferGeometry3",
-          component: () =>
-            import("@/views/WebApi/ThreeJSCustoms/BufferGeometry3/index.vue"),
+          component: () => import("@/views/WebApi/ThreeJSCustoms/BufferGeometry3/index.vue"),
           meta: {
             title: "缓冲模型 BufferGeometry之自定义颜色",
             imgSrc: "BufferGeometry3/BufferGeometry3.png",
@@ -605,8 +588,7 @@ const row: RouteRecordRaw = {
         {
           path: "BufferGeometry4",
           name: "WebApiThreeJSCustomsBufferGeometry4",
-          component: () =>
-            import("@/views/WebApi/ThreeJSCustoms/BufferGeometry4/index.vue"),
+          component: () => import("@/views/WebApi/ThreeJSCustoms/BufferGeometry4/index.vue"),
           meta: {
             title: "缓冲模型 BufferGeometry之颜色渐变柱状图",
             imgSrc: "BufferGeometry4/BufferGeometry4.png",
@@ -615,8 +597,7 @@ const row: RouteRecordRaw = {
         {
           path: "BufferGeometry5",
           name: "BufferGeometry5",
-          component: () =>
-            import("@/views/WebApi/ThreeJSCustoms/BufferGeometry5/index.vue"),
+          component: () => import("@/views/WebApi/ThreeJSCustoms/BufferGeometry5/index.vue"),
           meta: {
             title: "缓冲模型 BufferGeometry之索引",
             imgSrc: "BufferGeometry5/BufferGeometry5.jpg",
@@ -625,8 +606,7 @@ const row: RouteRecordRaw = {
         {
           path: "BufferGeometry6",
           name: "WebApiThreeJSCustomsBufferGeometry6",
-          component: () =>
-            import("@/views/WebApi/ThreeJSCustoms/BufferGeometry6/index.vue"),
+          component: () => import("@/views/WebApi/ThreeJSCustoms/BufferGeometry6/index.vue"),
           meta: {
             title: "缓冲模型 BufferGeometry之线",
             imgSrc: "BufferGeometry6/BufferGeometry6.jpg",
@@ -635,8 +615,7 @@ const row: RouteRecordRaw = {
         {
           path: "Normal",
           name: "WebApiThreeJSCustomsNormal",
-          component: () =>
-            import("@/views/WebApi/ThreeJSCustoms/Normal/index.vue"),
+          component: () => import("@/views/WebApi/ThreeJSCustoms/Normal/index.vue"),
           meta: {
             title: "法向量",
             // imgSrc: "BufferGeometry4/BufferGeometry4.png",
@@ -645,8 +624,7 @@ const row: RouteRecordRaw = {
         {
           path: "Ray",
           name: "WebApiThreeJSCustomsRay",
-          component: () =>
-            import("@/views/WebApi/ThreeJSCustoms/Ray/index.vue"),
+          component: () => import("@/views/WebApi/ThreeJSCustoms/Ray/index.vue"),
           meta: {
             title: "射线 Ray",
             // imgSrc: "Box3/Box3.png",
@@ -655,8 +633,7 @@ const row: RouteRecordRaw = {
         {
           path: "OrbitControls",
           name: "WebApiThreeJSCustomsOrbitControls",
-          component: () =>
-            import("@/views/WebApi/ThreeJSCustoms/OrbitControls/index.vue"),
+          component: () => import("@/views/WebApi/ThreeJSCustoms/OrbitControls/index.vue"),
           meta: {
             title: "OrbitControls轨道控制器",
             imgSrc: "OrbitControls/OrbitControls.jpg",
@@ -665,8 +642,7 @@ const row: RouteRecordRaw = {
         {
           path: "DRACOLoader",
           name: "WebApiThreeJSCustomsDRACOLoader",
-          component: () =>
-            import("@/views/WebApi/ThreeJSCustoms/DRACOLoader/index.vue"),
+          component: () => import("@/views/WebApi/ThreeJSCustoms/DRACOLoader/index.vue"),
           meta: {
             title: "加载器之解压加载器 DRACOLoader",
             imgSrc: "DRACOLoader/DRACOLoader.jpg",
---
+++ src/views/WebApi/Collection/IntersectionObserver/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>元素可见性</div>
+</template>
+
+<style scoped></style>
