052be709ba91a8667e4687d0b658040f66c60d87
付祖敏 <fuzumin@foxmail.com>
2025-11-11 22:49:54 +0800

    补充

--- src/views/VbenAdmin/Watermark/index.vue
+++ src/views/VbenAdmin/Watermark/index.vue
@@ -1,7 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// 创建水印
+const onCreate = () => {};
+
+// 更新水印
+const onUpdate = () => {};
+
+// 移除水印
+const onDel = () => {};
+</script>
 
 <template>
-  <div>水印</div>
+  <div>
+    <el-button @click="onCreate">创建水印</el-button>
+    <el-button type="primary" @click="onUpdate">更新水印</el-button>
+    <el-button type="danger" @click="onDel">移除水印</el-button>
+  </div>
 </template>
 
 <style scoped></style>
