efc855c7cef55bc3e95361f7c192a2ca4597d7c3
付祖敏 <fuzumin@foxmail.com>
2025-11-24 23:08:56 +0800

    添加

--- src/views/WebApi/SVG/Filter/Blur.vue
+++ src/views/WebApi/SVG/Filter/Blur.vue
@@ -1,20 +1,57 @@
 <script setup lang="ts"></script>
 
 <template>
-  <p>滤镜 filter</p>
+  <p>SourceGraphic</p>
   <svg width="400" height="400" view-box="0 0 100 100">
     <defs>
       <filter id="f1">
-        <feGaussianBlur in="SourceGraphic" stdDeviation="5" />
+        <feGaussianBlur in="SourceGraphic" stdDeviation="2" />
       </filter>
     </defs>
 
-    <rect x="10" y="10" width="50" height="50" fill="#f00"></rect>
-    <rect x="10" y="100" width="50" height="50" fill="#f00" filter="url(#f1)"></rect>
+    <image xlink:href="./Blur.png" x="0" y="0" height="100" width="100" filter="url(#f1)"></image>
+  </svg>
+
+  <p>SourceAlpha</p>
+  <svg width="400" height="400" view-box="0 0 100 100">
+    <defs>
+      <filter id="f2">
+        <feGaussianBlur in="SourceAlpha" stdDeviation="2" />
+      </filter>
+    </defs>
+
+    <image xlink:href="./Blur.png" x="0" y="0" height="100" width="100" filter="url(#f2)"></image>
+  </svg>
+
+  <p>对部分做模糊处理 x y width height</p>
+  <svg width="400" height="400" view-box="0 0 100 100">
+    <defs>
+      <filter id="f3">
+        <feGaussianBlur
+          in="SourceGraphic"
+          stdDeviation="5"
+          x="100"
+          y="0"
+          width="200"
+          height="200"
+          result="r3"
+        />
+
+        <feMerge>
+          <feMergeNode in="SourceGraphic"></feMergeNode>
+          <feMergeNode in="r3"></feMergeNode>
+        </feMerge>
+      </filter>
+    </defs>
+
+    <image xlink:href="./牛马.png" x="0" y="0" height="400" width="400" filter="url(#f3)"></image>
   </svg>
 </template>
 
 <style scoped>
+svg {
+  border: 1px solid red;
+}
 .rect {
   filter: url(#f1);
 }
